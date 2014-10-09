3065783459992e57440bda5a812fc83da29a47c4
Nevensky <neven.golenic@gmail.com>
2014-10-09 12:42:55 +0200

    version 0.2 pre-release

--- plotly.m
+++ plotly.m
@@ -1,15 +1,35 @@
 (* ::Package:: *)
 
-plotlyun=InputString["Type in your plotly username"];
+(*Plotly v0.2*)
 
 
-plotlykey=InputString["Type in your plotly key"];
+BeginPackage["Plotly`"];
+Plotly::usage = "A package for plotting plot.ly graphs in Wolfram Mathematica"
+
+
+Begin["`Private`"]
+
+
+plotlyun="Mathematica";
+
+
+plotlykey="u1ns4bx2df";
+
+
+PlotlyLogin[plotlyunlogin_,plotlykeylogin_]:=Set[{plotlyun,plotlykey},{plotlyunlogin,plotlykeylogin}];
 
 
 plotlyframed[d_]:=Hyperlink[Framed[Show[Import[StringJoin[StringReplace[d,{"\""->"","{"->"","}"->"","url"->"","message"~~__->"",Whitespace->"",","->"",":"->"","https"->"https:"}],".png"]],ImageSize->Large]],StringReplace[d,{"\""->"","{"->"","}"->"","url"->"","message"~~__->"",Whitespace->"",","->"",":"->"","https"->"https:"}]]
 
 
-Plotly[f_,{x_,min_,max_}]:=plotlyframed[URLExecute["https://plot.ly/clientresp",{"un"->plotlyun,"key"->plotlykey,"origin"->"plot","platform"->"mathematica",
+(* ::Input:: *)
+(*"Cot[x] ne radi! Pojedina\[CHacek]ni graf Log[x] ne radi"*)
+
+
+Options[Plotly]={PlotLabel->"a plot from the mathematica api"};
+
+
+Plotly[f_,{x_,min_,max_},OptionsPattern[]]:=plotlyframed[URLExecute["https://plot.ly/clientresp",{"un"->plotlyun,"key"->plotlykey,"origin"->"plot","platform"->"mathematica",
 "args"->StringReplace[
 If[StringCount[ToString[f],","]>0,
 StringJoin[
@@ -18,12 +38,12 @@ StringJoin["[{\"x\":",
 StringReplace[
 ToString[
 Table[x,{x,min,max,.1}]],
-{"{"->"[","}"->"]",".,"->".0,",".}"->".0]","Indeterminate"->"0",".e,"->".0,","e,"->",","e}"->"]",".e}"->".0]",".e"->".0e"}]
+{"{"->"[","}"->"]",".,"->".0,",".}"->".0]","Indeterminate"->"null",".e,"->".0,","e,"->",","e}"->"]",".e}"->".0]",".e"->".0e"}]
 ,",\"y\":",
 StringReplace[
 ToString[
 NumberForm[Table[f,{x,min,max,.1}][[All,i]],NumberFormat->(Row[{#1,"e",#3}]&)]],
-{"{"->"[","}"->"]",".,"->".0,",".}"->".0]","Indeterminate"->"0",".e,"->".0,","e,"->",","e}"->"]",".e}"->".0]",".e"->".0e"}]
+{"}"->"]",".,"->".0,",".}"->".0]",","~~ShortestMatch[___]~~"I"->", null","{"~~ShortestMatch[___]~~"I"->"[null","Indeterminate"->"null","-Infinity"->"null","Infinity"->"null","ComplexInfinity"->"null",".e,"->".0,","e,"->",","e}"->"]",".e}"->".0]",".e"->".0e","0.e"->"0.0","{"->"["}]
 ,",\"type\":\"scatter\"},"]
 ,{i,StringCount[ToString[f],","]+1}],
 "]"],
@@ -33,9 +53,9 @@ ToString[
 Table[x,{x,min,max,.1}]
 ],",\"y\":",
 ToString[
-NumberForm[Table[f,{x,min,max,.1}],NumberFormat->(Row[{#1,"e",#3}]&)]],",\"type\":\"scatter\"}"],{":{"->":[",":{"->"[","},\""->"],\"","}{"->"},{",".,"->".0,",".}"->".0]","Indeterminate"->".0,",".e,"->".0,","e,"->",","e}"->"]",".e}"->".0]",".e"->".0e"}],"]"]]
-,{",[{"->",{","},]"->"}]"}],
-"kwargs"->"{
+NumberForm[Table[f,{x,min,max,.1}],NumberFormat->(Row[{#1,"e",#3}]&)]],",\"type\":\"scatter\"}"],{":{"->":[",":{"->"[","},\""->"],\"","}{"->"},{",".,"->".0,",".}"->".0]",","~~ShortestMatch[___]~~"I"->", null","{"~~ShortestMatch[___]~~"I"->"[null","Indeterminate"->"null","ComplexInfinity"->"null","-Infinity"->"null","Infinity"->"null",".e,"->".0,","e,"->",","e}"->"]",".e}"->".0]",".e"->".0e","0.e"->"0.0"}],"]"]]
+,{",[{"->",{","},]"->"}]","nullndeterminat"->"null, null"}],
+"kwargs"->StringJoin["{
   \"filename\": \"mathematicaplot\",
   \"fileopt\": \"new\",
   \"style\": {
@@ -43,8 +63,14 @@ NumberForm[Table[f,{x,min,max,.1}],NumberFormat->(Row[{#1,"e",#3}]&)]],",\"type\
   },
   \"traces\": [0,1,2],
   \"layout\": {
-      \"title\": \"a plot from the mathematica api\"
+      \"title\": \"","a plot from the mathematica api v0.2","\"
   },
   \"world_readable\": true
-}"
+}"]
 },"Method"->"POST"]]
+
+
+End[]
+
+
+EndPackage[]
