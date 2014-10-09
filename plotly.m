69b47566524c8bac6ed5a7b75253878f4bebe5a1
Nevensky <neven.golenic@gmail.com>
2014-10-09 12:45:41 +0200

    v0.2 release
    Serious testing needed, possible issues related to wrong string
    replacements.
    StringReplace[] statements should be revised or rewritten from scratch
    in future versions.

--- plotly.m
+++ plotly.m
@@ -32,29 +32,42 @@ Options[Plotly]={PlotLabel->"a plot from the mathematica api"};
 Plotly[f_,{x_,min_,max_},OptionsPattern[]]:=plotlyframed[URLExecute["https://plot.ly/clientresp",{"un"->plotlyun,"key"->plotlykey,"origin"->"plot","platform"->"mathematica",
 "args"->StringReplace[
 If[StringCount[ToString[f],","]>0,
+
 StringJoin[
-Table[
-StringJoin["[{\"x\":",
-StringReplace[
-ToString[
-Table[x,{x,min,max,.1}]],
-{"{"->"[","}"->"]",".,"->".0,",".}"->".0]","Indeterminate"->"null",".e,"->".0,","e,"->",","e}"->"]",".e}"->".0]",".e"->".0e"}]
-,",\"y\":",
-StringReplace[
-ToString[
-NumberForm[Table[f,{x,min,max,.1}][[All,i]],NumberFormat->(Row[{#1,"e",#3}]&)]],
-{"}"->"]",".,"->".0,",".}"->".0]",","~~ShortestMatch[___]~~"I"->", null","{"~~ShortestMatch[___]~~"I"->"[null","Indeterminate"->"null","-Infinity"->"null","Infinity"->"null","ComplexInfinity"->"null",".e,"->".0,","e,"->",","e}"->"]",".e}"->".0]",".e"->".0e","0.e"->"0.0","{"->"["}]
+	Table[
+		StringJoin["[{\"x\":",
+			StringReplace[
+				ToString[
+			Table[x,{x,min,max,.1}]],
+			{"{"->"[","}"->"]",".,"->".0,",".}"->".0]","Indeterminate"->"null",".e,"->".0,","e,"->",","e}"->"]",".e}"->".0]",".e"->".0e"}]
+	,",\"y\":",
+		StringReplace[
+			ToString[
+				NumberForm[Table[f,{x,min,max,.1}][[All,i]],NumberFormat->(Row[{#1,"e",#3}]&)]],
+	{"}"->"]",".,"->".0,",".}"->".0]",","~~ShortestMatch[___]~~"I"->", null","{"~~ShortestMatch[___]~~"I"->"[null","Indeterminate"->"null","-Infinity"->"null","Infinity"->"null","ComplexInfinity"->"null",".e,"->".0,","e,"->",","e}"->"]",".e}"->".0]",".e"->".0e","0.e"->"0.0","{"->"["}]
 ,",\"type\":\"scatter\"},"]
 ,{i,StringCount[ToString[f],","]+1}],
 "]"],
-StringJoin[StringReplace[
-StringJoin["[{\"x\":",
-ToString[
-Table[x,{x,min,max,.1}]
-],",\"y\":",
-ToString[
-NumberForm[Table[f,{x,min,max,.1}],NumberFormat->(Row[{#1,"e",#3}]&)]],",\"type\":\"scatter\"}"],{":{"->":[",":{"->"[","},\""->"],\"","}{"->"},{",".,"->".0,",".}"->".0]",","~~ShortestMatch[___]~~"I"->", null","{"~~ShortestMatch[___]~~"I"->"[null","Indeterminate"->"null","ComplexInfinity"->"null","-Infinity"->"null","Infinity"->"null",".e,"->".0,","e,"->",","e}"->"]",".e}"->".0]",".e"->".0e","0.e"->"0.0"}],"]"]]
-,{",[{"->",{","},]"->"}]","nullndeterminat"->"null, null"}],
+StringJoin[
+
+		StringJoin["[{\"x\":",
+			StringReplace[
+		ToString[
+				Table[x,{x,min,max,.1}]],
+			{"{"->"[","}"->"]",".,"->".0,",".}"->".0]","Indeterminate"->"null",".e,"->".0,","e,"->",","e}"->"]",".e}"->".0]",".e"->".0e"}]
+,",\"y\":",
+			StringReplace[
+			ToString[
+						NumberForm[Table[f,{x,min,max,.1}],NumberFormat->(Row[{#1,"e",#3}]&)]]
+,
+
+{":{"->":[",":{"->"[","},\""->"],\"","}{"->"},{",".,"->".0,",".}"->".0]",","~~ShortestMatch[___]~~"I"->", null","{"~~ShortestMatch[___]~~"I"->"[null","Indeterminate"->"null","ComplexInfinity"->"null","-Infinity"->"null","Infinity"->"null",".e,"->".0,","e,"->",","e}"->"]",".e}"->".0]",".e"->".0e","0.e"->"0.0"}]
+
+,",\"type\":\"scatter\"}"]
+
+,"]"]]
+
+,{",[{"->",{","},]"->"}]","\"y\":{"->"\"y\":[","nullndeterminat"->"null, null"}],
 "kwargs"->StringJoin["{
   \"filename\": \"mathematicaplot\",
   \"fileopt\": \"new\",
